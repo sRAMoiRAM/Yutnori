e2de610c429b313357d9bd2cb1d8395ae1f6dc29
sRAMoiRAM <spiesma3@students.zhaw.ch>
2024-10-04 04:06:10 +0200

    style changes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let currentColorIndex = 0;
 button.addEventListener('click', function() {
     // Change the background color of the body
     document.body.style.backgroundColor = colors[currentColorIndex];
-    
+    document.body.style.color = colors[(currentColorIndex + 1) % colors.length];
     // Increment the color index, loop back to the start if at the end
     currentColorIndex = (currentColorIndex + 1) % colors.length;
 });
